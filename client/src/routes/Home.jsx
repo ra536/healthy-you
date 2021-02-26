f7aebd9229ad394c2e80f3a19e00fb2840965926
Jeffrey <jkk24@njit.edu>
2021-02-26 10:55:12 -0500

    connected frontend to backend to render list of test objects in db onto the client

--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -1,7 +1,41 @@
-import React from 'react'
+import React, { useEffect, useContext } from 'react';
+import TestAPI from '../apis/TestAPI';
+import { TestContext } from '../context/TestContext';
 
 const Home = () => {
-    return <div> Home </div>
+    // Store the data retrieved from backend API into context
+    const { test, setTest } = useContext(TestContext);
+
+    // Call our backend API to retrieve list of test objects from db
+    useEffect( () => {
+        // Define a function fetchData that calls APIs which is then called in useEffect
+        const fetchData = async () => {
+            try {
+                const response = await (TestAPI.get("/"));
+                setTest(response.data.data.test);
+            }
+            catch (err) {
+                console.log(err)
+            }
+        }
+        fetchData();
+    }, []);
+
+    return (
+        <div>
+            <h1>Home</h1>
+            <div>
+                {test.map(test => {
+                    return (
+                        <tr>
+                            <td>{ test.test_id }</td>
+                            <td>{ test.content }</td>
+                        </tr>
+                    )
+                })}
+            </div>
+        </div>
+    )
 }
 
 export default Home;
