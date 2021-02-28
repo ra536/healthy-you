4705a3084641b7ca37d5539277a1b613f05558f8
Robert Argasinski <robertargasinski@roberts-imac.myfiosgateway.com>
2021-02-27 21:15:55 -0500

    Attempt to fix Axios

--- client/src/apis/TestAPI.js
+++ client/src/apis/TestAPI.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export default axios.create({
     // Make sure you have the right server port
-    baseURL: process.env.BASE_URL,
+    baseURL: "http://localhost:8080/api/v1/test",
 });
