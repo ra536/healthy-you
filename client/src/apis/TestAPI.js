01677fa9c7f113e735ba84cb24db1450784971c8
Robert Argasinski <robertargasinski@roberts-imac.myfiosgateway.com>
2021-03-01 12:57:02 -0500

    Allow env varibles within TestAPI.js

--- client/src/apis/TestAPI.js
+++ client/src/apis/TestAPI.js
@@ -1,6 +1,11 @@
 import axios from 'axios';
 
+const host = process.env.HOST || "localhost";
+const port = process.env.PORT || "8080";
+console.log(host);
+console.log(port);
+
 export default axios.create({
     // Make sure you have the right server port
-    baseURL: "http://localhost:8080/api/v1/test",
+    baseURL: "http://" + host + ":" + port + "/api/v1/test",
 });
