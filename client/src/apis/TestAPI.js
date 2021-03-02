ea6d358968d30ec7cdd0e3aca9daabc8c2be7558
Robert Argasinski <robertargasinski@roberts-imac.myfiosgateway.com>
2021-03-01 23:03:30 -0500

    Hard code baseURL for Heroku

--- client/src/apis/TestAPI.js
+++ client/src/apis/TestAPI.js
@@ -3,5 +3,5 @@ require("dotenv").config();
 
 export default axios.create({
     // Make sure you have the right server port
-    baseURL: process.env.BASE_URL || "http://localhost:8080/api/v1/test",
+    baseURL: "https://healthy-you-project.herokuapp.com/api/v1/test" || "http://localhost:8080/api/v1/test",
 });
