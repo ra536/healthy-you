f7dfeb8e17ab7b834b62fd2ec759d2a522718248
Robert Argasinski <robertargasinski@roberts-imac.myfiosgateway.com>
2021-03-01 22:53:42 -0500

    Add dotenv

--- client/src/apis/TestAPI.js
+++ client/src/apis/TestAPI.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
+require("dotenv").config();
 
 export default axios.create({
     // Make sure you have the right server port
-    baseURL: process.env.BASE_URL,
+    baseURL: process.env.BASE_URL || "http://localhost:8080/api/v1/test",
 });
