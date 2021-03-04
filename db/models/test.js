61386da0816abcbf989f49aa372c1eebd2dede22
Jeffrey <jkk24@njit.edu>
2021-03-04 14:35:29 -0500

    created a test model

---
+++ db/models/test.js
@@ -0,0 +1,16 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const db = require('./index');
+
+const test = db.define('test', {
+    test_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true
+    },
+    content: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+})
+
+module.exports = test;
