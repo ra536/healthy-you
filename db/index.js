c86ad8d51cb14c6f7c6c5dde418d8bd13666a34a
Jeffrey <jkk24@njit.edu>
2021-03-04 20:00:37 -0500

    fixing ssl error

--- db/index.js
+++ db/index.js
@@ -2,10 +2,15 @@ require("dotenv").config();
 const { Sequelize } = require('sequelize');
 
 if (process.env.DATABASE_URL) {
-    module.exports = new Sequelize(process.env.DATABASE_URL)
+    module.exports = new Sequelize(process.env.DATABASE_URL, {
+        dialect: 'postgres',
+        dialectOptions: {
+            ssl: true
+        }
+    })
 } else {
     module.exports = new Sequelize(process.env.PGDATABASE, process.env.PGUSER, process.env.PGPASSWORD, {
         host: process.env.PGHOST,
         dialect: 'postgres'
     });
-}
+}  
