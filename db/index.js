4eaaa84d5142e2d4c07f4aac37451b0ea50e5cae
Jeffrey <jkk24@njit.edu>
2021-03-04 19:48:58 -0500

    updated db URI

--- db/index.js
+++ db/index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const { Sequelize } = require('sequelize');
 
 if (process.env.DATABASE_URL) {
-    const sequelize = new Sequelize(process.env.DATABASE_URL)
+    module.exports = new Sequelize(process.env.DATABASE_URL)
 } else {
     module.exports = new Sequelize(process.env.PGDATABASE, process.env.PGUSER, process.env.PGPASSWORD, {
         host: process.env.PGHOST,
