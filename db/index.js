c2b3dcdb883308b913e36e140161f34302712962
Jeffrey <jkk24@njit.edu>
2021-03-04 19:43:08 -0500

    added an heroku db uri

--- db/index.js
+++ db/index.js
@@ -1,7 +1,11 @@
 require("dotenv").config();
 const { Sequelize } = require('sequelize');
 
-module.exports = new Sequelize(process.env.PGDATABASE, process.env.PGUSER, process.env.PGPASSWORD, {
-    host: process.env.PGHOST,
-    dialect: 'postgres'
-});
+if (process.env.DATABASE_URL) {
+    const sequelize = new Sequelize(process.env.DATABASE_URL)
+} else {
+    module.exports = new Sequelize(process.env.PGDATABASE, process.env.PGUSER, process.env.PGPASSWORD, {
+        host: process.env.PGHOST,
+        dialect: 'postgres'
+    });
+}
