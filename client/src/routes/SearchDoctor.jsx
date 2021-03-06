45e2dff004741dbb2f469e6ba4ab678b549d1078
tnrusso <default>
2021-03-05 22:48:13 -0500

    get and display doctors based on search input

--- client/src/routes/SearchDoctor.jsx
+++ client/src/routes/SearchDoctor.jsx
@@ -2,11 +2,75 @@ import React, { useEffect, useContext, useState } from 'react';
 import SearchAPI from '../apis/SearchAPI'
 import { TestContext } from '../context/TestContext';
 
+
 const Search = () => {
+    const [specialty, setSpecialty] = useState("");
+    const [location, setLocation] = useState("");
+    const [doctor, setDoctor] = useState("");
+    const [rating, setRating] = useState("");
+
+    const { tests, setTests } = useContext(TestContext);
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            const response = await SearchAPI.post("/search", {
+                specialty: specialty,
+                address: location,
+                doctor_name: doctor,
+                rating: rating
+            })
+            setTests(response.data.data)
+            console.log(response.data.data)
+        }
+        catch (err) {
+            console.log(err)
+        }
+    }
 
     return(
         <div>
             <h1>Search</h1>
+            <form>
+                <input 
+                    id="search-specialty" 
+                    value={specialty} 
+                    placeholder="Specialty" 
+                    onChange={e => setSpecialty(e.target.value)} 
+                />
+                <input 
+                    id="search-location" 
+                    value={location} 
+                    placeholder="Location" 
+                    onChange={e => setLocation(e.target.value)} 
+                />
+                <input 
+                    id="search-doctor" 
+                    value={doctor} 
+                    placeholder="Doctor name" 
+                    onChange={e => setDoctor(e.target.value)} 
+                />
+                <input 
+                    id="search-rating" 
+                    value={rating} 
+                    placeholder="Rating" 
+                    onChange={e => setRating(e.target.value)} 
+                />
+                <button type="submit" onClick={handleSubmit}>
+                    Search
+                </button>
+            </form>
+                <div>
+                    {tests && tests.map((tests, index) => {
+                        return (
+                            <ol key={index}>
+                                <li>{ tests.doctor_name }</li>
+                                <li>{ tests.rating }</li>
+                            </ol>
+                        )
+                    })}
+                </div>
         </div>
     )
 };
--- routes/searchdoctor.js
+++ routes/searchdoctor.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db/index')
 const doctor = require('../db/models/doctor')
+const { Op } = require("sequelize");
 
 router.use(express.json());
 
@@ -20,6 +21,31 @@ router.get("/", async (req, res) => {
         console.error(err.message);
     }
 });
+
+router.post("/search", async (req, res) => {
+  var whereClause = {};
+  if(req.body.doctor_name != ""){
+    whereClause['doctor_name']={[Op.substring]: req.body.doctor_name};
+  }
+  if(req.body.rating != ""){
+    whereClause['rating']={[Op.gte]: req.body.rating};
+  }
+  try {
+      const doctors = await doctor.findAll({
+        raw: true,
+        where: whereClause
+      })
+      console.log(doctors.dataValues)
+      res.status(201).json({
+          status: "success",
+          data: doctors
+      })
+  }
+  catch (err) {
+    console.log(err)
+  }
+});
+
 
 
 module.exports = router;
