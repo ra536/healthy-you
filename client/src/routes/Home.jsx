86693e92d2b582aaf5726553548aab46c39d576b
Robert Argasinski <robertargasinski@roberts-imac.myfiosgateway.com>
2021-03-09 20:15:27 -0500

    Change NavBar color

--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -31,8 +31,10 @@ const Home = () => {
 
     return (
         <div>
-            <Navbar bg="dark" variant="dark" expand="lg">
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar bg="primary" variant="dark" expand="lg">
+                <Navbar.Brand href="#home">
+                    React-Bootstrap
+                </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
