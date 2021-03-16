d2dd124d2c3dcef44fd58833cdd865ebc00b7b59
mz342 <mz342@njit.edu>
2021-03-16 13:53:12 -0400

    basic blog page

---
+++ client/src/routes/Blog.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useContext } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+
+const Blog = () => {
+    // Store the data retrieved from backend API into context
+
+
+    // Call our backend API to retrieve list of test objects from db
+ 
+
+
+    return ( 
+        <h1>
+            blog page
+        </h1>
+
+    )
+}
+
+export default Blog;
