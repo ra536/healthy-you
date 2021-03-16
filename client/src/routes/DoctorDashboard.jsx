26a4a073d46c1237c3a825e88548987f8d91247e
Jeffrey <jkk24@njit.edu>
2021-03-16 13:35:52 -0400

    added sections to doctor dashboard

--- client/src/routes/DoctorDashboard.jsx
+++ client/src/routes/DoctorDashboard.jsx
@@ -1,32 +1,25 @@
 import React, { useEffect, useContext, useState } from 'react';
 import DashboardAPI from '../apis/DashboardAPI'
 import { TestContext } from '../context/TestContext';
-import InputNewDoc from '../components/InputNewDoc';
-import InputNewLoc from '../components/InputNewLoc';
+// import InputNewDoc from '../components/InputNewDoc';
+// import InputNewLoc from '../components/InputNewLoc';
 import InputNewPractice from '../components/InputNewPractice';
 import QueryDB from '../components/QueryDB';
 
 const DoctorDashboard = () => {
-    const { tests, setTests } = useContext(TestContext);
-
-    useEffect( () => {
-        // Define a function fetchData that calls APIs which is then called in useEffect
-        const fetchData = async () => {
-            try {
-                const response = await (DashboardAPI.get("/"));
-                console.log(response.data.data)
-                setTests(response.data.data);
-            }
-            catch (err) {
-                console.log(err)
-            }
-        }
-        fetchData();
-    }, []);    
-
     return(
         <div>
             <h1>Dashboard</h1>
+            <br/>
+            <h1>Profile Picture</h1>
+            <br/>
+            <h1>Phone Number</h1>
+            <br/>
+            <h1>Biography</h1>
+            <br/>
+            <h1>Specialties</h1>
+            <br/>
+            <h1>Practices</h1>
             <InputNewPractice/>
             <br/>
             <QueryDB/>
