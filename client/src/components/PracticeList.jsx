ea4ca5827150e22a5209ef8d15d61211e2a9e2a8
Jeffrey <jkk24@njit.edu>
2021-03-17 11:56:11 -0400

    updated front end to render list of practices in doctor dashboard on refresh

---
+++ client/src/components/PracticeList.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState, useContext } from 'react'
+import PracticeAPI from '../apis/PracticeAPI'
+import { TestContext } from '../context/TestContext';
+
+const PracticeList = (props) => {
+    const { practices, setPractices } = useContext(TestContext);
+    //const [practices, setPractices] = useState([]);
+
+    useEffect( () => {
+        // Define a function fetchData that calls APIs which is then called in useEffect
+        const fetchData = async () => {
+            try {
+                const response = await (PracticeAPI.post("/findAll", {
+                    doctorID: props.doctorID
+                }));
+                console.log(response.data.data)
+                setPractices(response.data.data)
+            }
+            catch (err) {
+                console.log(err)
+            }
+        }
+        fetchData();
+    }, []);
+
+    return (
+        <div>
+            {practices.map(practices => {
+                        return (
+                            <li key={practices.test_id}>
+                                { practices.name }
+                                { practices.website }
+                                { practices.social_media }
+                                { practices.location }
+                                { practices.phone }
+                                { practices.fax }
+                            </li>
+                        )
+                    })
+            }
+        </div>
+    )
+}
+
+export default PracticeList;
+
+
