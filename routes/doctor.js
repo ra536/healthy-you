7fd52e2c39dee3add7d6f52ffae2f2fb9e94a72d
Jeffrey <jkk24@njit.edu>
2021-03-18 19:22:05 -0400

    removed route to find specialty from doctor as it can be done from findDoctor

--- routes/doctor.js
+++ routes/doctor.js
@@ -52,23 +52,4 @@ router.post("/addSpecialty", async (req, res) => {
     }
 });
 
-router.post("/findSpecialty", async (req, res) => {
-    try {
-        const doctorResult = await doctor.findAll({
-            where: {
-                doctor_name: req.body.doctorID
-            },
-            raw: true
-        })
-        //console.log(req.body)
-        res.status(200).json({
-            status: "success",
-            data: doctorResult.specialty
-        })
-    }
-    catch (err) {
-      console.log(err)
-    }
-})
-
 module.exports = router;
