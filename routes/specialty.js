d461460e04deb5a50648badc6a66585e5f3ceb80
Jeffrey <jkk24@njit.edu>
2021-03-18 17:52:01 -0400

    created backend route to query all specialties

--- routes/specialty.js
+++ routes/specialty.js
@@ -6,41 +6,19 @@ const specialties = require('../db/models/specialty')
 router.use(express.json());
 
 //Test route to get started and gets all test objects from test table in db
-router.get("/", async (req, res) => {
+router.get("/findAll", async (req, res) => {
     try {
-        const testResults = await test.findAll({
+        const specialtyResults = await test.findAll({
             raw: true
         });
-        console.log(testResults);
+        console.log(specialtyResults);
         res.status(200).json({
           status: "success",
-          data: testResults
+          data: specialtyResults
         })
       } 
     catch (err) {
         console.error(err.message);
-    }
-});
-
-// Route to create a test object in DB
-router.post("/", async (req, res) => {
-    // Express JSON middleware allows for results to be in body
-    try {
-        const tests = await test.create({
-            test_id: req.body.test_id,
-            content: req.body.content,
-        })
-        console.log(tests.dataValues)
-        res.status(201).json({
-            status: "success",
-            data: {
-                test_id: tests.dataValues.test_id,
-                content: tests.dataValues.content
-            }
-        })
-    }
-    catch (err) {
-      console.log(err)
     }
 });
 
