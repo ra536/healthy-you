df79ee1c56441953b0daf1b2d141b81af9f714bb
Jeffrey <jkk24@njit.edu>
2021-03-25 19:01:50 -0400

    initial setup for react bootstrap + formik

--- client/src/components/UserRegisterForm.jsx
+++ client/src/components/UserRegisterForm.jsx
@@ -1,113 +1,65 @@
 import React, { useContext, useState } from 'react'
-import { useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
+import { Formik } from 'formik';
+import { Form, Button } from 'react-bootstrap'
 import UserAPI from '../apis/UserAPI'
 
 const schema = yup.object().shape({
-    username: yup.string().required(),
+    email: yup.string().email().required(),
     password: yup.string().min(4).max(16).required(),
     confirmPassword: yup.string().oneOf([yup.ref("password")]),
-    email: yup.string().email().required(),
     firstName: yup.string().required(),
     lastName: yup.string().required(),
     city: yup.string().required(),
-    state: yup.string().required(),
+    state: yup.string().required("You must enter your state"),
     birthdate: yup.string().required()
 })
 
 
 //Lets user input a test object into backend db
 const RegisterForm = () => {
-
-    const { register, handleSubmit, errors, setError } = useForm({
-        resolver: yupResolver(schema)
-    });
-
-    const submitForm = async (data) => {
-        console.log(data);
-        try {
-            const response = await UserAPI.post("/create", {
-                password: data.password,
-                email: data.email,
-                firstName: data.firstName,
-                lastName: data.lastName,
-                city: data.city,
-                state: data.state,
-                birthdate: data.birthdate
-            })
-            setError('email', {
-                type: "server",
-                message: response.data.status[0].message,
-            });
-        }
-        catch (err) {
-            console.log(err)
-        }
-    };
-
     return (
-        <form onSubmit={ handleSubmit(submitForm) }>
-            <div>
-            <input
-                    type="text"
-                    name="email"
-                    placeholder="Email Address"
-                    ref={ register }
-                />
-                <p> { errors.email && errors.email.message} </p>
-                <input
-                    type="password"
-                    name="password"
-                    placeholder="Password"
-                    ref={ register }
-                />
-                <p> { errors.password?.message } </p>
-                <input
-                    type="password"
-                    name="confirmPassword"
-                    placeholder="Confirm Password"
-                    ref={ register }
-                />
-                <p> { errors.confirmPassword && "Passwords don't match" } </p>
-                <input
-                    type="text"
-                    name="firstName"
-                    placeholder="First Name"
-                    ref={ register }
-                />
-                <p> { errors.firstName?.message } </p>
-                <input
-                    type="text"
-                    name="lastName"
-                    placeholder="Last Name"
-                    ref={ register }
-                />
-                <p> { errors.lastName?.message } </p>
-                <input
-                    type="text"
-                    name="city"
-                    placeholder="City"
-                    ref={ register }
-                />
-                <p> { errors.city?.message } </p>
-                <input
-                    type="text"
-                    name="state"
-                    placeholder="State"
-                    ref={ register }
-                />
-                <p> { errors.state?.message } </p>
-                <input
-                    type="text"
-                    name="birthdate"
-                    placeholder="Birthdate MM/DD/YYYY"
-                    ref={ register }
-                />
-                <p> { errors.birthdate?.message } </p>
-                <input type="submit" id="submit" />
-            </div>
-        </form>
+        <Formik
+            initialValues={{
+                email: "",
+                password: "",
+                confirmPassword: "",
+                firstName: "",
+                lastName: "",
+                city: "",
+                state: "",
+                birthdate: ""
+            }}
+            onSubmit={(data) => console.log(data)}
+        >
+            {({
+                handleSubmit,
+                handleChange,
+                handleBlur,
+                values
+            }) => {
+                return (
+                    <Form onSubmit={ handleSubmit }>
+                        <Form.Group controlId="formEmail">
+                        <Form.Label> Email </Form.Label>
+                        <Form.Control
+                            type="text"
+                            name="email"
+                            values={ values.email }
+                            onChange={ handleChange }
+                            onBlue={ handleBlur }
+                            placeholder="Enter your email!"
+                        />
+                        <Form.Control.Feedback>
+                        </Form.Control.Feedback>
+                        </Form.Group>
+                        <Button type="submit">
+                            Submit
+                        </Button>
+                    </Form>
+                )
+            }}
+        </Formik>
     )
 }
 
