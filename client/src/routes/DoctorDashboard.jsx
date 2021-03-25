ebddabb5d6f08ff92ba566cfdd1a386b1fd49493
tnrusso <default>
2021-03-25 00:21:00 -0400

    let doctors update username, profile pic, phone num, and bio from dashboard

--- client/src/routes/DoctorDashboard.jsx
+++ client/src/routes/DoctorDashboard.jsx
@@ -10,7 +10,7 @@ import { Container, Table } from 'react-bootstrap';
 import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
 import { Logout } from '../components/LogoutButton';
 import { LoginContext } from '../context/LoginPersistence';
-
+import axios from 'axios';
 
 const DoctorDashboard = (props) => {
     let { doctorID } = useParams();
@@ -19,6 +19,15 @@ const DoctorDashboard = (props) => {
     const [rating, setRating] = useState();
     const [name, setName] = useState();
     const [profilePicture, setProfilePicture] = useState();
+    const [bio, setBio] = useState("");
+    const [phone, setPhone] = useState("");
+
+    const [newImage, setNewImage] = useState(""); // image link
+    const [updatedName, setUpdatedName] = useState("");
+    const [updatedPhone, setUpdatedPhone] = useState("");
+    const [updatedBio, setUpdatedBio] = useState("");
+
+
     // const [specialties, setSpecialties] = useState();
     // const [doctorID, setDoctorID] = useState("");
     const { specialties, setSpecialties } = useContext(AppContext);
@@ -35,6 +44,8 @@ const DoctorDashboard = (props) => {
                 setName(response.data.data[0].doctor_name)
                 setProfilePicture(response.data.data[0].profile_picture)
                 setSpecialties(response.data.data[0].specialty)
+                setBio(response.data.data[0].bio)
+                setPhone(response.data.data[0].phone)
                 // setDoctorID(response.data.data[0].doctor_id)
             }
             catch (err) {
@@ -48,6 +59,83 @@ const DoctorDashboard = (props) => {
     //         <p>Authentication error: Unauthorized</p>
     //     )
     // };
+
+    const previewImage = async (e) => {
+        var reader = new FileReader();
+        
+        reader.addEventListener("load", function() {
+            var image = new Image();
+            image.height = 100;
+            image.title = "Name";
+            image.src = this.result;
+            document.getElementById('input-file').appendChild(image);
+            setNewImage(this.result);
+            console.log(this.result);
+            console.log(typeof this.result);
+        }, false);
+
+        reader.readAsDataURL(e.target.files[0]);
+    }
+
+    const handleSubmitName = async (e) => {
+        e.preventDefault();
+        console.log(updatedName);
+        try{
+            const response = await (DoctorAPI.post("/updateName", {
+                doctor_id: doctorID,
+                name: updatedName
+            }));
+            console.log(response.data.data);
+        }
+        catch(err){
+            console.log(err);
+        }
+    }
+
+    const handleSubmitPhone = async (e) => {
+        e.preventDefault();
+        console.log(updatedPhone);
+        try{
+            const response = await (DoctorAPI.post("/updatePhone", {
+                doctor_id: doctorID,
+                phone: updatedPhone
+            }));
+            console.log(response.data.data);
+        }
+        catch(err){
+            console.log(err);
+        }
+    }
+
+    const handleSubmitBio = async (e) => {
+        e.preventDefault();
+        console.log(updatedBio);
+        try{
+            const response = await (DoctorAPI.post("/updateBio", {
+                doctor_id: doctorID,
+                bio: updatedBio
+            }));
+            console.log(response.data.data);
+        }
+        catch(err){
+            console.log(err);
+        }
+    }
+
+    const handleSubmitProfilePic = async (e) => {
+        e.preventDefault();
+        console.log(newImage);
+        try{
+            const response = await (DoctorAPI.post("/updateProfilePic", {
+                doctor_id: doctorID,
+                image: newImage
+            }));
+            console.log(response.data.data);
+        }
+        catch(err){
+            console.log(err);
+        }
+    }
 
     return (
         <>
@@ -90,14 +178,61 @@ const DoctorDashboard = (props) => {
             <br />
             <h1>Name</h1>
             { name}
-            <br />
+            <br/>
+            <form>
+                <input
+                id="input-new-name"
+                value={updatedName}
+                placeholder="Name"
+                onChange={e => setUpdatedName(e.target.value)}
+                />
+                <button type="submit" onClick={handleSubmitName}>Save</button>
+            </form>
+            
+            <br /><br />
             <h1>Profile Picture</h1>
-            { profilePicture}
-            <br />
+            <img src={profilePicture} width="200px" id="preview"></img>
+            <form>
+                <input
+                    id="input-file"
+                    name="article-image"
+                    type="file"
+                    onChange={e => previewImage(e)}
+                />
+                <br/>
+                <button type="submit" onClick={handleSubmitProfilePic}>Save</button>
+            </form>
+            <img src={newImage} width="200px" id="preview"></img>
+            <br /><br />
             <h1>Phone Number</h1>
+            {phone}
             <br />
+            <form>
+                <input
+                    id="input-new-phone"
+                    value={updatedPhone}
+                    placeholder="Phone Number"
+                    onChange={e => setUpdatedPhone(e.target.value)}
+                />
+                <button type="submit" onClick={handleSubmitPhone}>Save</button>
+            </form>
+            
+            <br /><br />
             <h1>Biography</h1>
+            {bio}
             <br />
+            <form>
+                <textarea rows="10" cols="75"
+                    id="input-bio"
+                    value={updatedBio}
+                    placeholder="Biography"
+                    onChange={e => setUpdatedBio(e.target.value)}
+                />
+                <br/>
+                <button type="submit" onClick={handleSubmitBio}>Save</button>
+            </form>
+            
+            <br /><br />
             <h1>Specialties</h1>
             <Table striped bordered hover>
                 <thead>
@@ -106,9 +241,9 @@ const DoctorDashboard = (props) => {
                     </tr>
                 </thead>
                 <tbody>
-                    {specialties.map(specialties => {
+                    {specialties.map((specialties,index) => {
                         return (
-                            <tr>
+                            <tr key={index}>
                                 <td>{specialties}</td>
                             </tr>
                         )
