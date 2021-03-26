a708fe7d313236d53085bebe350deeeae1fd133a
Jeffrey <jkk24@njit.edu>
2021-03-26 04:45:25 -0400

    updated ProtectedRoute

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,6 +1,22 @@
 import React from 'react'
 import { Route, Redirect } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext'
 
-const ProtectedRoute = () => {
+const ProtectedRoute = ({ component: Component, requiredRole: requiredRole, ...rest }) => {
+    const { role } = useAuth()
 
+    return (
+        <Route
+            {...rest}
+            render={props => {
+                if (role == requiredRole) {
+                    return <Component {...props}/>
+                } else {
+                    <Redirect to="/" />
+                }
+            }}
+        >
+
+        </Route>
+    )
 }
