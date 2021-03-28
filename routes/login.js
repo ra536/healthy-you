5be41c5fca82ec2debe557ce494814a4ab4d86b6
Jeffrey <jkk24@njit.edu>
2021-03-27 20:26:05 -0400

    changed backend login routes

--- routes/login.js
+++ routes/login.js
@@ -5,80 +5,94 @@ const user = require('../db/models/user');
 const doctor = require('../db/models/doctor');
 const writer = require('../db/models/writer');
 const bcrypt = require('bcrypt');
+const passport = require('passport');
 const cookieParser = require('cookie-parser');
 const { createUserToken, createDoctorToken, createWriterToken } = require('../JWT');
 
 router.use(express.json());
 
-router.post("/", async (req, res) => {
-  // destructing properties from req.body
-  const { email, password, role } = req.body;
+router.post("/login", passport.authenticate('local'), async (req, res, next) => {
+  res.json({
+    status: "Authenticated"
+  })
+});
 
-  if (role == "User") {
-    const userResult = await user.findOne({
-      where: {
-          email: email
-      }
-    })
-    if (!userResult) {
-      res.json({
-        target: "email",
-        status: "User does not exist!"
-      })
-    }
-    const dbPassword = userResult.password
-    bcrypt.compare(password, dbPassword).then((match) => {
-      if (!match) {
-        res.json({
-          target: "password",
-          status: "Password is incorrect!"
-        })
-      } else {
-          const accessToken = createUserToken(userResult)
-          res.cookie("user-access-token", accessToken, {
-            maxAge: 60 * 60 * 24 * 30 * 1000,
-            httpOnly: true
-          })
-          res.status(200).json({
-          status: "success",
-        })
-      }
-    })
-  } 
-  else if (role == "Doctor") {
-    const doctorResult = await doctor.findAll({
-      where: {
-          email: email
-      }
-    })
-    if (!doctorResult) {
-      res.json({
-        status: "Doctor does not exist!",
-      })
-    }
-    res.status(200).json({
-      status: "success",
-      data: doctorResult
-    })
-  } 
-  else {
-    const writerResult = await writer.findAll({
-      where: {
-          email: email
-      }
-    })
-    if (!writerResult) {
-      res.json({
-        status: "Writer does not exist!",
-      })
-    }
-    res.status(200).json({
-      status: "success",
-      data: writerResult
-    })
-  }
-
+router.get("/user", (req, res) => {
+  res.json({
+    status: "Authenticated",
+    user: req.user
+  })
 })
+
+// router.post("/", async (req, res) => {
+//   // destructing properties from req.body
+//   const { email, password, role } = req.body;
+
+//   if (role == "User") {
+//     const userResult = await user.findOne({
+//       where: {
+//           email: email
+//       }
+//     })
+//     if (!userResult) {
+//       res.json({
+//         target: "email",
+//         status: "User does not exist!"
+//       })
+//     }
+//     const dbPassword = userResult.password
+//     bcrypt.compare(password, dbPassword).then((match) => {
+//       if (!match) {
+//         res.json({
+//           target: "password",
+//           status: "Password is incorrect!"
+//         })
+//       } else {
+//           const accessToken = createUserToken(userResult)
+//           res.cookie("user-access-token", accessToken, {
+//             maxAge: 60 * 60 * 24 * 30 * 1000,
+//             httpOnly: true
+//           })
+//           res.status(200).json({
+//           status: "success",
+//         })
+//       }
+//     })
+//   } 
+//   else if (role == "Doctor") {
+//     const doctorResult = await doctor.findAll({
+//       where: {
+//           email: email
+//       }
+//     })
+//     if (!doctorResult) {
+//       res.json({
+//         status: "Doctor does not exist!",
+//       })
+//     }
+//     res.status(200).json({
+//       status: "success",
+//       data: doctorResult
+//     })
+//   } 
+//   else {
+//     const writerResult = await writer.findAll({
+//       where: {
+//           email: email
+//       }
+//     })
+//     if (!writerResult) {
+//       res.json({
+//         status: "Writer does not exist!",
+//       })
+//     }
+//     res.status(200).json({
+//       status: "success",
+//       data: writerResult
+//     })
+//   }
+
+// })
 
 // router.get("/:id", async (req, res) =>{
 //     try {
