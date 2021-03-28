a953c493fee46805518e5000f076665dcfae7d1f
Jeffrey <jkk24@njit.edu>
2021-03-28 18:02:06 -0400

    renamed frontend registration components

---
+++ client/src/routes/Registration.jsx
@@ -0,0 +1,21 @@
+import React, { useContext } from 'react';
+import RegistrationForm from '../components/RegistrationForm'
+import { Redirect } from 'react-router-dom';
+import { AuthContext } from '../context/AuthContext';
+
+const Registration = () => {
+    const { loggedIn } = useContext(AuthContext);
+    return loggedIn == true ?
+        (
+            <Redirect to="/" />
+        )
+        :
+        (
+            <div>
+                <h1>Login</h1>
+                <RegistrationForm />
+            </div>
+        )
+}
+
+export default Registration;
