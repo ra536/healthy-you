8289e939a6d8245cc3dd3a976edda934f32cbca7
Jeffrey <jkk24@njit.edu>
2021-03-30 12:38:49 -0400

    removed chunks of code that have been commented out in backend routes

--- routes/writer.js
+++ routes/writer.js
@@ -5,41 +5,6 @@ const writers = require('../db/models/writer.js')
 
 router.use(express.json());
 
-// router.post("/create", async (req, res) => {
-//     try {
-//         if(req.body.practiceName != ""){
-//             const practices = await practice.create({
-//                 username: req.body.username,
-//                 password: req.body.password,
-//                 email: req.body.email,
-//                 first_name: req.body.socialMedia,
-//                 last_name: req.body.address,
-//                 email: req.body.phone,
-//                 city: req.body.fax,
-//                 state: req.body.doctorID,
-//                 birthdate: req.body.doctorID
-//             })
-//             console.log(practices.dataValues)
-//             res.status(201).json({
-//                 status: "success",
-//                 // data: {
-//                 //     practice_id: practices.dataValues.practice_id,
-//                 //     name: practices.dataValues.name,
-//                 //     location: practices.dataValues.location,
-//                 //     website: practices.dataValues.website,
-//                 //     social_media: practices.dataValues.social_media,
-//                 //     address: practices.dataValues.address,
-//                 //     phone: practices.dataValues.phone,
-//                 //     fax: practices.dataValues.fax
-//                 // }
-//             })
-//         }
-//     }
-//     catch (err) {
-//       console.log(err)
-//     }
-// });
-
 router.post("/create", async (req, res) => {
     try {
         const users = await user.create({
