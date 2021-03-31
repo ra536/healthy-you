216a78faf6e10c93322890dc7594c422a9b9ee69
Jeffrey <jkk24@njit.edu>
2021-03-31 07:26:48 -0400

    es linted passport.js

--- passport.js
+++ passport.js
@@ -1,12 +1,11 @@
-//Passport Authentication functions
+// Passport Authentication functions
 const isAuthAndAdmin = (req, res, next) => {
   if (req.isAuthenticated() && req.user.role === "Admin") {
     return next();
-  } else {
-    return res.json({
-      status: "You are not authenticated!",
-    });
   }
+  return res.json({
+    status: "You are not authenticated!",
+  });
 };
 
 function isAuthAndUser(req, res, next) {
@@ -15,10 +14,8 @@ function isAuthAndUser(req, res, next) {
     (req.user.role === "Admin" || req.user.role === "User")
   ) {
     return next();
-  } else {
-    console.log("You are not authenticated!");
-    return res.json({ status: "You are not authenticated!" });
   }
+  return res.json({ status: "You are not authenticated!" });
 }
 
 function isAuthAndDoctor(req, res, next) {
@@ -27,10 +24,9 @@ function isAuthAndDoctor(req, res, next) {
     (req.user.role === "Admin" || req.user.role === "Doctor")
   ) {
     return next();
-  } else {
-    console.log("You are not authenticated!");
-    return res.json({ status: "You are not authenticated!" });
   }
+  // console.log("You are not authenticated!");
+  return res.json({ status: "You are not authenticated!" });
 }
 
 function isAuthAndWriter(req, res, next) {
@@ -39,10 +35,9 @@ function isAuthAndWriter(req, res, next) {
     (req.user.role === "Admin" || req.user.role === "Writer")
   ) {
     return next();
-  } else {
-    console.log("You are not authenticated!");
-    return res.json({ status: "You are not authenticated!" });
   }
+  // console.log("You are not authenticated!");
+  return res.json({ status: "You are not authenticated!" });
 }
 
 module.exports = {
