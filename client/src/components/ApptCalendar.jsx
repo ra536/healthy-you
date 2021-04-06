430e7f9e85945dc3858c529cb646b5b46a493092
tnrusso <default>
2021-04-06 14:20:37 -0400

    add section to display appt info when clicked, remove unnecessary function

--- client/src/components/ApptCalendar.jsx
+++ client/src/components/ApptCalendar.jsx
@@ -13,15 +13,14 @@ const ApptCalendar = (props) => {
     const [currentDayApts, setCurrentDayApts] = useState([]);
     const [selectedDay, setDay] = useState(new Date());
 
-    function loadTodaysAppts(props){
+    function loadTodaysAppts(props, td) {
         // To display the appointments for today when page loads
         setCurrentDayApts([]);
 
         var todaysAppts = [];
         var index = 0;
-        var td = selectedDay;
 
-        for(var i = 0; i < props.length; i++){
+        for (var i = 0; i < props.length; i++) {
             // sdt = Start Date Time
             var sdt = new Date(props[i].start_time);
             var edt = new Date(props[i].end_time);
@@ -31,23 +30,23 @@ const ApptCalendar = (props) => {
             var month = sdt.getMonth();
             var year = sdt.getFullYear();
 
-            if(date == td.getDate() && month == td.getMonth() && year == td.getFullYear()){
+            if (date == td.getDate() && month == td.getMonth() && year == td.getFullYear()) {
                 var startHours = sdt.getHours()
-                if(startHours.toString().length === 1){
-                    startHours= "0" + startHours;
+                if (startHours.toString().length === 1) {
+                    startHours = "0" + startHours;
                 }
                 var startMinutes = sdt.getMinutes();
-                if(startMinutes.toString().length === 1){
+                if (startMinutes.toString().length === 1) {
                     startMinutes = "0" + startMinutes;
                 }
                 var startTime = "Start: " + startHours + ":" + startMinutes;
-        
+
                 var endHours = edt.getHours()
-                if(endHours.toString().length === 1){
-                    endHours= "0" + endHours;
+                if (endHours.toString().length === 1) {
+                    endHours = "0" + endHours;
                 }
                 var endMinutes = edt.getMinutes();
-                if(endMinutes.toString().length === 1){
+                if (endMinutes.toString().length === 1) {
                     endMinutes = "0" + endMinutes;
                 }
                 var endTime = " End: " + endHours + ":" + endMinutes;
@@ -65,45 +64,7 @@ const ApptCalendar = (props) => {
         // When user clicks a day on the calendar, show appointments for that day
         setCurrentDayApts([]);
         setDay(e);
-        var todaysAppts = [];
-        var index = 0;
-
-        for(var i = 0; i < appts.length; i++){
-            var id = appts[i].appointment_id;
-            var sdt = new Date(appts[i].start_time);
-            var edt = new Date(appts[i].end_time);
-            
-            var date = sdt.getDate();
-            var month = sdt.getMonth();
-            var year = sdt.getFullYear();
-
-            if(date === e.getDate() && month === e.getMonth() && year === e.getFullYear()){
-                var startHours = sdt.getHours()
-                if(startHours.toString().length === 1){
-                    startHours= "0" + startHours;
-                }
-                var startMinutes = sdt.getMinutes();
-                if(startMinutes.toString().length === 1){
-                    startMinutes = "0" + startMinutes;
-                }
-                var startTime = "Start: " + startHours + ":" + startMinutes;
-
-                var endHours = edt.getHours()
-                if(endHours.toString().length === 1){
-                    endHours= "0" + endHours;
-                }
-                var endMinutes = edt.getMinutes();
-                if(endMinutes.toString().length === 1){
-                    endMinutes = "0" + endMinutes;
-                }
-                var endTime = " End: " + endHours + ":" + endMinutes;
-
-                todaysAppts[index] = [startTime, endTime, id];
-                index += 1;
-            }
-        }
-        todaysAppts.sort();
-        setCurrentDayApts(todaysAppts);
+        loadTodaysAppts(appts, e);
     }
 
     useEffect(() => {
@@ -114,7 +75,7 @@ const ApptCalendar = (props) => {
                 }));
                 // console.log(response.data.data);
                 setAppointments(response.data.data)
-                loadTodaysAppts(response.data.data);
+                loadTodaysAppts(response.data.data,selectedDay);
             }
             catch (err) {
                 console.log(err)
@@ -127,30 +88,45 @@ const ApptCalendar = (props) => {
         console.log(e);
     }
 
-    return(
-        <div style={{display: "flex"}}>
-            <div >
-                <Calendar
-                    onChange={onChange}
-                    value={value}
-                    calendarType="US"
-                    minDetail="year"
-                    onClickDay={dayClicked}
-                />
-            </div>
-            <div style={{alignItems:"center" , display: "flex", flexDirection:"column", left:350, overflow:"hidden", "borderWidth":"1px", 'borderColor':"#aaaaaa", 'borderStyle':'solid', width:1000}}>
-                <h4>Availability</h4>
-                <div style={{overflowY: 'auto'}}>
-                    {currentDayApts.map((currentDayApts, index) => {
-                        return (
-                            <Button onClick={() => handleClick(currentDayApts[2])} key={index} style={{margin:5}}>
-                                {currentDayApts[0] + currentDayApts[1]}
-                            </Button>
-                        )
-                    })}
+    return (
+        <div>
+            <div style={{ display: "flex" }}>
+                <div >
+                    <Calendar
+                        onChange={onChange}
+                        value={value}
+                        calendarType="US"
+                        minDetail="year"
+                        onClickDay={dayClicked}
+                    />
+                </div>
+                <div style={{ alignItems: "center", display: "flex", flexDirection: "column", left: 350, overflow: "hidden", "borderWidth": "1px", 'borderColor': "#aaaaaa", 'borderStyle': 'solid', width: 1000 }}>
+                    <h4>Availability</h4>
+                    <div style={{ overflowY: 'auto' }}>
+                        {currentDayApts.map((currentDayApts, index) => {
+                            return (
+                                <Button onClick={() => handleClick(currentDayApts[2])} key={index} style={{ margin: 5 }}>
+                                    {currentDayApts[0] + currentDayApts[1]}
+                                </Button>
+                            )
+                        })}
+                    </div>
                 </div>
             </div>
-        </div> 
+            <div>
+                <h1>Appointment Info</h1>
+                Practice:
+                <br />
+                Start Time:
+                <br />
+                End Time:
+                <br />
+                Status:
+                <br />
+                Patient:
+                <br />
+            </div>
+        </div>
     );
 };
 
