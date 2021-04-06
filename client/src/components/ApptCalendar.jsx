62211d8d6790d8031397cf489192cc831f91428d
tnrusso <default>
2021-04-05 23:29:04 -0400

    rework calendar to show start and end times

--- client/src/components/ApptCalendar.jsx
+++ client/src/components/ApptCalendar.jsx
@@ -3,6 +3,7 @@ import { Container, Table, Button, Col, Row } from 'react-bootstrap';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import DoctorAPI from '../apis/DoctorAPI';
+import AppointmentAPI from '../apis/AppointmentAPI';
 
 const ApptCalendar = (props) => {
     // console.log(props.doctorID);
@@ -14,32 +15,48 @@ const ApptCalendar = (props) => {
     function loadTodaysAppts(props){
         // To display the appointments for today when page loads
         setCurrentDayApts([]);
+
         var todaysAppts = [];
         var index = 0;
         var td = new Date();
 
         for(var i = 0; i < props.length; i++){
-            var stringAsDate = new Date(props[i])
-            var date = stringAsDate.getDate();
-            var month = stringAsDate.getMonth();
-            var year = stringAsDate.getFullYear();
+            // sdt = Start Date Time
+            var sdt = new Date(props[i].start_time);
+            var edt = new Date(props[i].end_time);
+            var id = props[i].appointment_id;
 
-            if(date === td.getDate() && month === td.getMonth() && year === td.getFullYear()){
-                var hours = stringAsDate.getHours()
-                if(hours.toString().length === 1){
-                    hours= "0" + hours;
+            var date = sdt.getDate();
+            var month = sdt.getMonth();
+            var year = sdt.getFullYear();
+
+            if(date == td.getDate() && month == td.getMonth() && year == td.getFullYear()){
+                var startHours = sdt.getHours()
+                if(startHours.toString().length === 1){
+                    startHours= "0" + startHours;
                 }
-                var minutes = stringAsDate.getMinutes();
-                if(minutes.toString().length === 1){
-                    minutes = "0" + minutes;
+                var startMinutes = sdt.getMinutes();
+                if(startMinutes.toString().length === 1){
+                    startMinutes = "0" + startMinutes;
                 }
-                var apptTime = hours + ":" + minutes;
-                todaysAppts[index] = apptTime;
+                var startTime = "Start: " + startHours + ":" + startMinutes;
+        
+                var endHours = edt.getHours()
+                if(endHours.toString().length === 1){
+                    endHours= "0" + endHours;
+                }
+                var endMinutes = edt.getMinutes();
+                if(endMinutes.toString().length === 1){
+                    endMinutes = "0" + endMinutes;
+                }
+                var endTime = " End: " + endHours + ":" + endMinutes;
+
+                todaysAppts[index] = [startTime, endTime, id];
                 index += 1;
             }
         }
-
         todaysAppts.sort();
+        console.log(todaysAppts);
         setCurrentDayApts(todaysAppts);
     }
 
@@ -48,43 +65,53 @@ const ApptCalendar = (props) => {
         setCurrentDayApts([]);
         var todaysAppts = [];
         var index = 0;
-
         for(var i = 0; i < appts.length; i++){
-            var stringAsDate = new Date(appts[i])
-            var date = stringAsDate.getDate();
-            var month = stringAsDate.getMonth();
-            var year = stringAsDate.getFullYear();
+            var id = appts[i].appointment_id;
+            var sdt = new Date(appts[i].start_time);
+            var edt = new Date(appts[i].end_time);
+            
+            var date = sdt.getDate();
+            var month = sdt.getMonth();
+            var year = sdt.getFullYear();
 
             if(date === e.getDate() && month === e.getMonth() && year === e.getFullYear()){
-                var hours = stringAsDate.getHours()
-                if(hours.toString().length === 1){
-                    hours= "0" + hours;
+                var startHours = sdt.getHours()
+                if(startHours.toString().length === 1){
+                    startHours= "0" + startHours;
                 }
-                var minutes = stringAsDate.getMinutes();
-                if(minutes.toString().length === 1){
-                    minutes = "0" + minutes;
+                var startMinutes = sdt.getMinutes();
+                if(startMinutes.toString().length === 1){
+                    startMinutes = "0" + startMinutes;
                 }
-                var apptTime = hours + ":" + minutes;
-                todaysAppts[index] = apptTime;
+                var startTime = "Start: " + startHours + ":" + startMinutes;
+
+                var endHours = edt.getHours()
+                if(endHours.toString().length === 1){
+                    endHours= "0" + endHours;
+                }
+                var endMinutes = edt.getMinutes();
+                if(endMinutes.toString().length === 1){
+                    endMinutes = "0" + endMinutes;
+                }
+                var endTime = " End: " + endHours + ":" + endMinutes;
+
+                todaysAppts[index] = [startTime, endTime, id];
                 index += 1;
             }
         }
-
         todaysAppts.sort();
-        console.log(todaysAppts.length);
         setCurrentDayApts(todaysAppts);
     }
 
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await (DoctorAPI.post("/getAppointments", {
+                const response = await (AppointmentAPI.post("/getAppointments", {
                     doctor_id: props.doctorID
                 }));
-                // console.log(response.data.data[0])
-                // console.log(response.data.data[0].available_appt)
-                setAppointments(response.data.data[0].available_appt)
-                loadTodaysAppts(response.data.data[0].available_appt);
+                console.log(response.data.data);
+                setAppointments(response.data.data)
+                loadTodaysAppts(response.data.data);
             }
             catch (err) {
                 console.log(err)
@@ -108,13 +135,13 @@ const ApptCalendar = (props) => {
                     onClickDay={dayClicked}
                 />
             </div>
-            <div style={{alignItems:"center" , display: "flex", flexDirection:"column", left:350, overflow:"hidden", "borderWidth":"1px", 'borderColor':"#aaaaaa", 'borderStyle':'solid', width:243, height:302}}>
+            <div style={{alignItems:"center" , display: "flex", flexDirection:"column", left:350, overflow:"hidden", "borderWidth":"1px", 'borderColor':"#aaaaaa", 'borderStyle':'solid', width:1000}}>
                 <h4>Availability</h4>
                 <div style={{overflowY: 'auto'}}>
                     {currentDayApts.map((currentDayApts, index) => {
                         return (
-                            <Button onClick={() => handleClick(currentDayApts)} key={index} style={{margin:5}}>
-                                {currentDayApts}
+                            <Button onClick={() => handleClick(currentDayApts[2])} key={index} style={{margin:5}}>
+                                {currentDayApts[0] + currentDayApts[1]}
                             </Button>
                         )
                     })}
