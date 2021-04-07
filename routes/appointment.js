949ff9b7bc90797410d69c4ca499253fb66a70c8
tnrusso <default>
2021-04-07 14:25:47 -0400

    add check to prevent scheduling appts with overlapping times

--- routes/appointment.js
+++ routes/appointment.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const { check } = require("prettier");
 
 const router = express.Router();
 const db = require('../db/index')
 const appointment = require('../db/models/appointment')
 const doctor = require('./doctor');
+const { Op } = require("sequelize");
 
 router.use(express.json());
 
@@ -44,85 +46,106 @@ router.post("/", async (req, res) => {
 });
 
 router.post("/saveAppt", async (req, res) => {
-    try {
-        console.log(req.body.time);
-        const appts = await appointment.create(
-            {
-                doctor_id: req.body.doctor_id,
-                start_time: req.body.start,
-                end_time: req.body.end,
-                status: 0
-            }
-           );
-        console.log(req.body)
-        res.status(200).json({
-            status: "success",
-            data: {
-                appointment_id: appts.dataValues.appointment_id,
-                start_time: req.body.start,
-                end_time: req.body.end,
-                doctor_id: req.body.doctor_id,
-                status: 0,
-            }
-        })
+  try {
+    const checkIfExists = await appointment.count({
+      where: {
+        doctor_id: req.body.doctor_id,
+        [Op.or]: {
+          start_time: {
+            [Op.and]: [{
+              [Op.gte]: req.body.start,
+              [Op.lte]: req.body.end
+            }]
+          },
+          end_time: {
+            [Op.and]: [{
+              [Op.gte]: req.body.start,
+              [Op.lte]: req.body.end
+            }]
+          }
+        },
+      }
+    })
+    console.log(checkIfExists)
+    if (checkIfExists == 0) {
+      const appts = await appointment.create(
+        {
+          doctor_id: req.body.doctor_id,
+          start_time: req.body.start,
+          end_time: req.body.end,
+          status: 0
+        }
+      );
+      // console.log(req.body)
+      res.status(200).json({
+        status: "success",
+        data: {
+          appointment_id: appts.dataValues.appointment_id,
+          start_time: req.body.start,
+          end_time: req.body.end,
+          doctor_id: req.body.doctor_id,
+          status: 0,
+        }
+      })
     }
-    catch (err) {
-      console.log(err)
-    }
+  }
+  catch (err) {
+    console.log(err)
+  }
 });
 
 router.post("/getAllAppts", async (req, res) => {
-    try {
-        const appointmentResults = await appointment.findAll({
-            raw: true,
-            where: {doctor_id: req.body.doctor_id}
-        });
-        console.log(appointmentResults);
-        res.status(200).json({
-          status: "success",
-          data: appointmentResults
-        })
-      } 
-    catch (err) {
-        console.error(err);
-    }
+  try {
+    const appointmentResults = await appointment.findAll({
+      raw: true,
+      where: { doctor_id: req.body.doctor_id }
+    });
+    // console.log(appointmentResults);
+    res.status(200).json({
+      status: "success",
+      data: appointmentResults
+    })
+  }
+  catch (err) {
+    console.error(err);
+  }
 });
 
 router.post("/getApptInfo", async (req, res) => {
   try {
-      const appointmentResults = await appointment.findAll({
-          raw: true,
-          where: {
-            appointment_id: req.body.appointment_id
-          }
-      });
-      console.log(appointmentResults);
-      res.status(200).json({
-        status: "success",
-        data: appointmentResults
-      })
-    } 
+    const appointmentResults = await appointment.findAll({
+      raw: true,
+      where: {
+        appointment_id: req.body.appointment_id
+      }
+    });
+    // console.log(appointmentResults);
+    res.status(200).json({
+      status: "success",
+      data: appointmentResults
+    })
+  }
   catch (err) {
-      console.error(err);
+    console.error(err);
   }
 });
 
 router.post("/cancelAppt", async (req, res) => {
   try {
-      const appointmentResults = await appointment.destroy({
-          raw: true,
-          where: {
-            appointment_id: req.body.appointment_id
-          }
-      });
-      console.log(appointmentResults);
-      res.status(200).json({
-        status: "success",
-        data: appointmentResults
-      })
-    } 
+    const appointmentResults = await appointment.destroy({
+      raw: true,
+      where: {
+        appointment_id: req.body.appointment_id
+      }
+    });
+    // console.log(appointmentResults);
+    res.status(200).json({
+      status: "success",
+      data: appointmentResults
+    })
+  }
   catch (err) {
-      console.error(err);
+    console.error(err);
   }
 });
 
