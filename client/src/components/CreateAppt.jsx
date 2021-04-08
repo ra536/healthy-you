07b66065029055aab2286a52c60e3b32799378b3
tnrusso <default>
2021-04-08 14:17:02 -0400

    create options to save 1 or all unpublished appts and to remove unsaved appt

--- client/src/components/CreateAppt.jsx
+++ client/src/components/CreateAppt.jsx
@@ -10,26 +10,100 @@ const CreateAppt = (props) => {
     const [startDT, setStartDT] = useState(new Date());
     const [endDT, setEndDT] = useState(new Date());
 
-    const [listOfDTs, insertDT] = useState([]);
+    const [unSavedAppts, setUnsavedAppts] = useState([]);
 
-    const addToList = async () => {
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await (AppointmentAPI.post("/getAllAppts", {
+                    doctor_id: props.doctorID,
+                    status: [-1]
+                }));
+                // console.log(response.data.data);
+                setUnsavedAppts(response.data.data);
+            }
+            catch (err) {
+                console.log(err)
+            }
+        }
+        fetchData();
+    }, [props]);
+
+    const onClickInsert = async () => {
         console.log(startDT, "\n\n", endDT);
         try {
-            if(startDT <= endDT){
-                const response = await AppointmentAPI.post("/saveAppt", {
-                start: startDT,
-                end: endDT,
-                doctor_id: props.doctorID
+            if (startDT <= endDT) {
+                const response = await AppointmentAPI.post("/createAppt", {
+                    start: startDT,
+                    end: endDT,
+                    doctor_id: props.doctorID
                 });
                 console.log(response.data.data)
-                insertDT([...listOfDTs, [startDT, endDT, response.data.data.appointment_id]]);
-                props.newAppt([startDT,endDT,response.data.data.appointment_id]);
+                setUnsavedAppts([...unSavedAppts, response.data.data])
             }
         }
         catch (err) {
             console.log(err)
         }
-        // insertDT([...listOfDTs, [startDT, endDT]]);
+    }
+
+    const onClickSaveAll = async () => {
+        console.log(unSavedAppts)
+        const response = await AppointmentAPI.put("/saveAllAppts", {
+            doctor_id: props.doctorID
+        })
+        console.log(response.data.data);
+        props.newAppt(unSavedAppts)
+        setUnsavedAppts([]);
+    }
+
+    const onClickSave = async (data) => {
+        console.log(data.appointment_id);
+        const response = await AppointmentAPI.put("/saveOneAppt", {
+            doctor_id: props.doctorID,
+            appointment_id: data.appointment_id
+        })
+        console.log(response.data.data);
+        props.newAppt(data)
+        setUnsavedAppts(unSavedAppts.filter(item => item.appointment_id !== data.appointment_id))
+    }
+
+    const onClickRemove = async (id) => {
+        console.log(id)
+        try {
+            const response = await AppointmentAPI.post("/cancelAppt", {
+                doctor_id: props.doctorID,
+                appointment_id: id
+            });
+            console.log(response.data.data)
+            setUnsavedAppts(unSavedAppts.filter(item => item.appointment_id !== id))
+        }
+        catch (err) {
+            console.log(err)
+        }
+    }
+
+    const formatDT = (dt) => {
+        dt = new Date(dt);
+        var ampm = "AM"
+        var hours = dt.getHours();
+        var mins = dt.getMinutes();
+
+        if (hours === 0) {
+            hours = 12;
+        } else if (hours >= 11 && hours < 24) {
+            ampm = "PM";
+            if(hours > 12){
+                hours = hours - 12;
+            }
+        }
+        if(hours.toString().length === 1){
+            hours = "0" + hours.toString();
+        }
+        if (mins.toString().length === 1) {
+            mins = "0" + mins.toString();
+        }
+        return (dt.getMonth() + 1) + "/" + dt.getDate() + "/" + dt.getFullYear() + " " + hours + ":" + mins + " " + ampm;
     }
 
     return (
@@ -54,8 +128,28 @@ const CreateAppt = (props) => {
                     {endDT.toLocaleString()}
                 </div>
                 <div style={{ margin: 55 }}>
-                    <Button size="sm" onClick={() => addToList()}>Insert</Button>
+                    <Button size="sm" onClick={() => onClickInsert()}>Insert</Button>
                 </div>
+
+            </div>
+            <div>
+                <h3>Unsaved Appointments (Start -- End)</h3>
+                <Button size="sm" onClick={() => onClickSaveAll()}>Save All</Button>
+                <ul style={{ listStyleType: "none" }}>
+                    {unSavedAppts.map((unSavedAppts, index) => {
+                        return (
+                            <li key={index}>
+                                {formatDT(unSavedAppts.start_time)} -- {formatDT(unSavedAppts.end_time)}
+                                <Button style={{ margin: 3 }} onClick={() => onClickSave(unSavedAppts)}>
+                                    ✓
+                                </Button>
+                                <Button style={{ margin: 3 }} onClick={() => onClickRemove(unSavedAppts.appointment_id)}>
+                                    X
+                                </Button>
+                            </li>
+                        )
+                    })}
+                </ul>
             </div>
         </div>
     );
