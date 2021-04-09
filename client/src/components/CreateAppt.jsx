74a09575dc00841b5576ae537832414f3a90e5d6
tnrusso <default>
2021-04-09 14:30:13 -0400

    separate list of unpublished appts from create appt component

--- client/src/components/CreateAppt.jsx
+++ client/src/components/CreateAppt.jsx
@@ -10,26 +10,8 @@ const CreateAppt = (props) => {
     const [startDT, setStartDT] = useState(new Date());
     const [endDT, setEndDT] = useState(new Date());
 
-    const [unSavedAppts, setUnsavedAppts] = useState([]);
     const [allPractices, setAllPractices] = useState([]);
     const [selectedPractice, setSelectedPractice] = useState();
-
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await (AppointmentAPI.post("/getAllAppts", {
-                    doctor_id: props.doctorID,
-                    status: [-1]
-                }));
-                // console.log(response.data.data);
-                setUnsavedAppts(response.data.data);
-            }
-            catch (err) {
-                console.log(err)
-            }
-        }
-        fetchData();
-    }, [props]);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -60,44 +42,8 @@ const CreateAppt = (props) => {
                     doctor_id: props.doctorID
                 });
                 console.log(response.data.data)
-                setUnsavedAppts([...unSavedAppts, response.data.data])
+                props.newAppt(response.data.data)
             }
-        }
-        catch (err) {
-            console.log(err)
-        }
-    }
-
-    const onClickSaveAll = async () => {
-        console.log(unSavedAppts)
-        const response = await AppointmentAPI.put("/saveAllAppts", {
-            doctor_id: props.doctorID
-        })
-        console.log(response.data.data);
-        props.newAppt(unSavedAppts)
-        setUnsavedAppts([]);
-    }
-
-    const onClickSave = async (data) => {
-        console.log(data.appointment_id);
-        const response = await AppointmentAPI.put("/saveOneAppt", {
-            doctor_id: props.doctorID,
-            appointment_id: data.appointment_id
-        })
-        console.log(response.data.data);
-        props.newAppt(data)
-        setUnsavedAppts(unSavedAppts.filter(item => item.appointment_id !== data.appointment_id))
-    }
-
-    const onClickRemove = async (id) => {
-        console.log(id)
-        try {
-            const response = await AppointmentAPI.post("/cancelAppt", {
-                doctor_id: props.doctorID,
-                appointment_id: id
-            });
-            console.log(response.data.data)
-            setUnsavedAppts(unSavedAppts.filter(item => item.appointment_id !== id))
         }
         catch (err) {
             console.log(err)
@@ -161,25 +107,6 @@ const CreateAppt = (props) => {
                 <div style={{ margin: 55 }}>
                     <Button size="sm" onClick={() => onClickInsert()}>Insert</Button>
                 </div>
-            </div>
-            <div>
-                <h3>Unsaved Appointments (Start -- End)</h3>
-                <Button size="sm" onClick={onClickSaveAll}>Save All</Button>
-                <ul style={{ listStyleType: "none" }}>
-                    {unSavedAppts.map((unSavedAppts, index) => {
-                        return (
-                            <li key={index}>
-                                {formatDT(unSavedAppts.start_time)} -- {formatDT(unSavedAppts.end_time)}
-                                <Button style={{ margin: 3 }} onClick={() => onClickSave(unSavedAppts)}>
-                                    ✓
-                                </Button>
-                                <Button style={{ margin: 3 }} onClick={() => onClickRemove(unSavedAppts.appointment_id)}>
-                                    X
-                                </Button>
-                            </li>
-                        )
-                    })}
-                </ul>
             </div>
         </div>
     );
