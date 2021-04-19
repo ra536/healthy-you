088222870128b6e0c78d763ced959d6b70abd14f
Robert Argasinski <robertargasinski@roberts-imac.myfiosgateway.com>
2021-04-19 15:13:35 -0400

    Add Heroku URL for appointment route

--- client/src/apis/AppointmentAPI.js
+++ client/src/apis/AppointmentAPI.js
@@ -4,5 +4,5 @@ require("dotenv").config();
 export default axios.create({
     // Make sure you have the right server port
     // production url "https://healthy-you-project.herokuapp.com/api/v1/test"
-    baseURL: "http://localhost:8080/api/v1/appointment",
+    baseURL: process.env.REACT_APP_APPOINTMENT_URL || "http://localhost:8080/api/v1/appointment",
 });
