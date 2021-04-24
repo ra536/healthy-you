6d434003c5ce03dd43fb5236bd10e743b617053b
Robert Argasinski <robertargasinski@roberts-imac.myfiosgateway.com>
2021-04-24 18:23:25 -0400

    Fix Category URL for Heroku

--- client/src/apis/CategoryAPI.js
+++ client/src/apis/CategoryAPI.js
@@ -5,5 +5,5 @@ export default axios.create({
   // Make sure you have the right server port
   // production url "https://healthy-you-project.herokuapp.com/api/v1/test"
   baseURL:
-    "http://localhost:8080/api/v1/category",
+    process.env.REACT_APP_CATEGORY_URL || "http://localhost:8080/api/v1/category",
 });
