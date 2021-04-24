78cf93fe91e4a8d2ce77e938a5671778a74c533c
Robert Argasinski <robertargasinski@roberts-imac.myfiosgateway.com>
2021-04-24 18:22:37 -0400

    Add modal to Health Guide to pick category search

--- client/src/components/HealthGuide.jsx
+++ client/src/components/HealthGuide.jsx
@@ -1,8 +1,30 @@
-import React from "react";
-import { Container, Row, Col, Button } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { Container, Row, Col, Button, Modal } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
+import CategoryAPI from "../apis/CategoryAPI";
 
 const HealthGuide = (props) => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    // Define a function fetchData that calls APIs which is then called in useEffect
+    const fetchData = async () => {
+      try {
+        const response = await CategoryAPI.get("/findAll");
+        console.log(response.data.data)
+        setCategories(response.data.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <div style={{ backgroundColor: "#F8F8F8" }}>
       <Container id="health-guide">
@@ -39,7 +61,7 @@ const HealthGuide = (props) => {
               </Button>
             </Col>
             <Col>
-              <Button variant="success" size="md" block href="/results">
+              <Button variant="success" size="md" block onClick={handleShow}>
                 More...
               </Button>
             </Col>
@@ -47,6 +69,27 @@ const HealthGuide = (props) => {
             </Col>
           </Row>
         </Container>
+      </Container>
+      <Container>
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Pick a Category</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+    {categories.map((category) => {
+      return (
+        <>
+        <Button href={"/results/?practice=&doctor=&specialty=&category="+category.category+"&location=&rating="} variant="outline-success">{category.category}</Button>{' '}
+        </>
+      );
+    })}
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+          </Button>
+          </Modal.Footer>
+        </Modal>
       </Container>
       <br />
       <br />
