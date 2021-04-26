e2439e16c3403d1ffb5cf9cd9b8875f405ecb7bb
Jeffrey <jkk24@njit.edu>
2021-04-26 14:28:25 -0400

    updated social icons to link to the correct article

--- client/src/routes/Article.jsx
+++ client/src/routes/Article.jsx
@@ -23,10 +23,10 @@ const Article = (props) => {
   const [author, setAuthor] = useState("");
   const [writerID, setWriterID] = useState(null);
   const [numViews, setNumViews] = useState(0);
+  const [link, setLink] = useState(null);
 
-
-  const link =
-    "https://healthy-you-project.herokuapp.com/article/87918716-f71f-4548-aea3-ad0496d44c9a";
+  // const link =
+  //   "https://healthy-you-project.herokuapp.com/article/87918716-f71f-4548-aea3-ad0496d44c9a";
 
   useEffect(() => {
     // Define a function fetchData that calls APIs which is then called in useEffect
@@ -36,11 +36,9 @@ const Article = (props) => {
           article_id: id,
         });
 
-        const views = await ArticleAPI.post("/pageView",{
+        const views = await ArticleAPI.post("/pageView", {
           id: id,
         });
-        console.log("data", views.data.data[0])
-
         setHeadline(response.data.data[0].headline);
         setCategory(response.data.data[0].category);
         setSummary(response.data.data[0].summary);
@@ -53,6 +51,7 @@ const Article = (props) => {
           response.data.writer.firstName + " " + response.data.writer.lastName
         );
         setWriterID(response.data.data[0].writer_id);
+        setLink("https://healthy-you-project.herokuapp.com/article/" + id);
         console.log(response.data.data[0]);
       } catch (err) {
         console.log(err);
@@ -68,43 +67,44 @@ const Article = (props) => {
     <>
       <TopNavBar />
       <Container>
-      <Row>
-            <Col xs={12} md={8}>
-        <h1>{headline}</h1>
-        Category: <a href={"/category/" + category}>{category}</a>
-        <br />
-        <br />
-        <a href={"/writer-profile/" + writerID}>{author}</a> |{" "}
-        <Moment format="dddd MMMM Do, YYYY [at] h:mm A">{publishDate}</Moment> |{" "}
-        {numViews} views
-        <br />
-        <br />
-        <SocialShareButtons link={link} />
-        <br />
-        <br />
-        <img src={image} width="100%" alt="" />
-        <p>
-          <i>Above: {caption}</i>
-        </p>
-        <br />
-        <p>
-          {content.split("\n").map((i, key) => {
-            return <p key={key}>{i}</p>;
-          })}
-        </p>
-        <br />
-        <br />
-        <br />
-        </Col>
-            <Col xs={6} md={4}>
-            
-          <br />
-          <HomeSideBar />
-          <br />
+        <Row>
+          <Col xs={12} md={8}>
+            <h1>{headline}</h1>
+            Category: <a href={"/category/" + category}>{category}</a>
+            <br />
+            <br />
+            <a href={"/writer-profile/" + writerID}>{author}</a> |{" "}
+            <Moment format="dddd MMMM Do, YYYY [at] h:mm A">
+              {publishDate}
+            </Moment>{" "}
+            | {numViews} views
+            <br />
+            <br />
+            <SocialShareButtons link={link} />
+            <br />
+            <br />
+            <img src={image} width="100%" alt="" />
+            <p>
+              <i>Above: {caption}</i>
+            </p>
+            <br />
+            <p>
+              {content.split("\n").map((i, key) => {
+                return <p key={key}>{i}</p>;
+              })}
+            </p>
+            <br />
+            <br />
+            <br />
           </Col>
-          </Row>
-        </Container>
-        <Footer />
+          <Col xs={6} md={4}>
+            <br />
+            <HomeSideBar />
+            <br />
+          </Col>
+        </Row>
+      </Container>
+      <Footer />
     </>
   );
 };
