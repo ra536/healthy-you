efa9962875ccc72131ed5c2b30a329c956b5155c
Robert Argasinski <robertargasinski@roberts-imac.myfiosgateway.com>
2021-04-26 14:40:47 -0400

    Change Heroku URL

--- client/src/apis/ImageAPI.js
+++ client/src/apis/ImageAPI.js
@@ -5,7 +5,7 @@ export default axios.create({
   // Make sure you have the right server port
   // production url "https://healthy-you-project.herokuapp.com/api/v1/test"
   baseURL:
-    process.env.REACT_APP_IMAGE_API || "http://localhost:8080/api/v1/image",
+    process.env.REACT_APP_IMAGE_URL || "http://localhost:8080/api/v1/image",
 });
 
 
