c5c69f609a26644b7f7fd14b436526996dea8bfd
Joseph Cayemitte <josephcayemitte@gmail.com>
2021-04-26 22:05:16 -0400

    filter blogs from latest article, addded related articles to the bottom of articles

--- client/src/routes/Article.jsx
+++ client/src/routes/Article.jsx
@@ -9,6 +9,7 @@ import SocialShareButtons from "../components/SocialShareButtons";
 import "bootstrap/dist/css/bootstrap.css";
 import HomeSideBar from "../components/HomeSideBar";
 import Footer from "../components/Footer";
+import ListGroup from 'react-bootstrap/ListGroup'
 
 const Article = (props) => {
   let { id } = useParams();
@@ -23,7 +24,12 @@ const Article = (props) => {
   const [author, setAuthor] = useState("");
   const [writerID, setWriterID] = useState(null);
   const [numViews, setNumViews] = useState(0);
-
+  const [sameAuthor, setSameAuthor] = useState([]);
+  const [authorLinks, setAuthorLinks] = useState([])
+  const [authorImages, setAuthorImages] = useState([])
+  const [sameCategory, setSameCategory] = useState([]);
+  const [categoryLinks, setCategoryLinks] = useState([])
+  const [categoryImages, setCategoryImages] = useState([])
 
   const link =
     "https://healthy-you-project.herokuapp.com/article/87918716-f71f-4548-aea3-ad0496d44c9a";
@@ -39,6 +45,17 @@ const Article = (props) => {
         const views = await ArticleAPI.post("/pageView",{
           id: id,
         });
+
+        const relatedAuthor = await ArticleAPI.post("/author", {
+          article_id: id,
+          numOfArticles: 3
+        })
+
+        const relatedCategory = await ArticleAPI.post("/sameCategory", {
+          article_id: id,
+          numOfArticles: 3
+        })
+
         console.log("data", views.data.data[0])
 
         setHeadline(response.data.data[0].headline);
@@ -49,11 +66,25 @@ const Article = (props) => {
         setCaption(response.data.data[0].image_caption);
         setPublishDate(response.data.data[0].createdAt);
         setNumViews(response.data.data[0].page_views);
-        setAuthor(
-          response.data.writer.firstName + " " + response.data.writer.lastName
-        );
+        // setAuthor(
+        //   response.data.writer.firstName + " " + response.data.writer.lastName
+        // );
         setWriterID(response.data.data[0].writer_id);
-        console.log(response.data.data[0]);
+
+        for(var i = 0; i < (relatedAuthor.data.data.length); i++)
+        {
+          setSameAuthor(oldList => [...oldList, relatedAuthor.data.data[i].headline])
+          setAuthorLinks(oldList => [...oldList,  "/article/" + relatedAuthor.data.data[i].article_id])
+          setAuthorImages(oldList => [...oldList, relatedAuthor.data.data[i].image_data])
+        }
+        // setSameAuthor(relatedAuthor.data.data)
+        for(var i = 0; i < (relatedCategory.data.data.length); i++)
+        {
+          setSameCategory(oldList => [...oldList, relatedCategory.data.data[i].headline])
+          setCategoryLinks(oldList => [...oldList, "/article/" + relatedCategory.data.data[i].article_id])
+          setCategoryImages(oldList => [...oldList, relatedCategory.data.data[i].image_data])
+        }
+
       } catch (err) {
         console.log(err);
       }
@@ -86,23 +117,48 @@ const Article = (props) => {
         </p>
         <br />
         <p>
-          {content.split("\n").map((i, key) => {
-            return <p key={key}>{i}</p>;
-          })}
+            {content.split("\n").map((i, key) => {
+              return <p key={key}>{i}</p>;
+            })}
         </p>
         <br />
         <br />
         <br />
+
+        <ListGroup horizontal>
+          {sameAuthor.map((i,key) => {
+            return( 
+              <> 
+                <img  src={authorImages[key]} />
+                <ListGroup.Item key={key}> <a  href = {authorLinks[key]}> {i} </a> </ListGroup.Item> 
+              </>
+            )
+          })}
+        </ListGroup>
+        <br />
+
+        <ListGroup horizontal>
+        {sameCategory.map((j, ckey) => {
+            return (
+              <> 
+                <img  src={categoryImages[ckey]} />
+                <ListGroup.Item key={ckey}> <a href = {categoryLinks[ckey]} > {j} </a></ListGroup.Item>
+              </>
+            )
+          })}
+
+        </ListGroup>
+
       </Container>
       <Container
           id="Right Sidebar"
           style={{ width: "35%", display: "inline-block" }}
         >
-          <br />
+          <br /> 
           <HomeSideBar />
           <br />
         </Container>
-        <Footer />
+        <Footer />   
     </>
   );
 };
