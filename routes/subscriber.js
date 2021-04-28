e964b6efc83f06e4eb7f3bb643dc3384908fc182
Jeffrey <jkk24@njit.edu>
2021-04-28 19:40:49 -0400

    an email is now sent to the subsriber and the admin when someone subscribes

--- routes/subscriber.js
+++ routes/subscriber.js
@@ -1,31 +1,53 @@
 const express = require("express");
 
 const router = express.Router();
-const multer = require("multer");
-const subscriber  = require("../db/models/subscriber.js");
-const { Sequelize, Op } = require("sequelize");
+const nodemailer = require("nodemailer");
+const subscriber = require("../db/models/subscriber.js");
 
 router.use(express.json());
 
+// gmail transporter
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+});
+
 router.post("/", async (req, res) => {
-    try {
-        const {
-            email,
-            firstName,
-            lastName,
-          } = req.body;
-        const subscriberResult = await subscriber.create({
-            email: email,
-            firstName: firstName,
-            lastName: lastName,
-        });
-        res.status(200).json({
-            status: "success",
-            data: subscriberResult.email,
-          });    
-    } catch (error) {
-        console.log(error.message)
-    }
+  const { firstName, lastName } = req.body;
+  try {
+    const subscriberResult = await subscriber.create({
+      email: req.body.email,
+      firstName: firstName,
+      lastName: lastName,
+    });
+    res.status(200).json({
+      status: "success",
+      data: subscriberResult.email,
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+
+  const mailOptionsAdmin = {
+    from: `${firstName} + " " ${lastName}`, // sender address
+    to: process.env.EMAIL,
+    subject: `${firstName} ${lastName} Has Subscribed to Our Newsletter!`, // Subject line
+    text: `${firstName} ${lastName} Has Subscribed to Our Newsletter!`, // plain text body
+    replyTo: `${firstName} ${lastName} Has Subscribed to Our Newsletter!`,
+  };
+  const mailOptionsSubscriber = {
+    from: "Healthy You", // sender address
+    to: `${req.body.email}`,
+    subject: "Thank you for subscribing to our newsletter!", // Subject line
+    text: "Thank you for subscribing to our newsletter!", // plain text body
+    replyTo: "Thank you for subscribing to our newsletter!",
+  };
+  // send mail with defined transport object
+  transporter.sendMail(mailOptionsAdmin);
+  transporter.sendMail(mailOptionsSubscriber);
 });
 
 module.exports = router;
