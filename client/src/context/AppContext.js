1a80849ef143233dc973b017d50651299441d21d
Jeffrey <jkk24@njit.edu>
2021-08-28 20:08:35 -0400

    updated context to accomadate order pages

--- client/src/context/AppContext.js
+++ client/src/context/AppContext.js
@@ -7,6 +7,55 @@ export const AppContextProvider = (props) => {
 
   //updates state with new array when adding a test object
   //this is to have the client render a new test object automatically without having to refresh page
+
+  const [confirming, setConfirming] = useState(false);
+  const [printingOptions, setPrintingOptions] = useState([
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+  ]);
+  const [typeOfAd, setTypeOfAd] = useState([]);
+  const [digitalServices, setDigitalServices] = useState([
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+  ]);
+  const [advertisingDuration, setAdvertisingDuration] = useState([]);
+  const [user, setUser] = useState(null);
+  const [onlineAdvertising, setOnlineAdvertising] = useState([
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+  ]);
+  const [onlineType, setOnlineType] = useState([]);
+  const [webDesignComments, setWebDesignComments] = useState("");
+  const [webHostingComments, setWebHostingComments] = useState("");
+  const [webDesignTotal, setWebDesignTotal] = useState(0);
+  const [webHostingTotal, setWebHostingTotal] = useState(0);
+  const [comments, setComments] = useState("");
+
   const addTest = (test) => {
     setTests([...tests, test]);
   };
@@ -129,6 +178,32 @@ export const AppContextProvider = (props) => {
         setCategories,
         addCategory,
         removeCategory,
+        confirming,
+        setConfirming,
+        printingOptions,
+        setPrintingOptions,
+        typeOfAd,
+        setTypeOfAd,
+        digitalServices,
+        setDigitalServices,
+        advertisingDuration,
+        setAdvertisingDuration,
+        user,
+        setUser,
+        onlineAdvertising,
+        setOnlineAdvertising,
+        onlineType,
+        setOnlineType,
+        webDesignComments,
+        setWebDesignComments,
+        webHostingComments,
+        setWebHostingComments,
+        webDesignTotal,
+        setWebDesignTotal,
+        webHostingTotal,
+        setWebHostingTotal,
+        comments,
+        setComments,
       }}
     >
       {props.children}
